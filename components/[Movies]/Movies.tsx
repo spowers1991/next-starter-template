d0fa8ce00224779dfa0590b749502da095bb0dd7
Steven Powers <stevenpowers1991@gmail.com>
2025-12-31 02:31:42 +0000

    fix build errors

--- components/[Movies]/Movies.tsx
+++ components/[Movies]/Movies.tsx
@@ -3,13 +3,12 @@
 import React from "react";
 import Link from "next/link";
 import type { Movie } from "@/services/[Movies]/{Movie}/types/Movie";
-import type { Post } from "@/lib/sanity/types/Post";
 import H1 from "../{H1}/H1";
 import Section from "../{Section}/Section";
 import Grid from "../{Grid}/Grid";
 
 interface MovieProps {
-  movies: Post[]; 
+  movies: Movie[]; 
 }
 
 export default function Movies({ movies }: MovieProps) {
--- components/[Persons]/Persons.tsx
+++ components/[Persons]/Persons.tsx
@@ -6,7 +6,6 @@ import H1 from "@/components/{H1}/H1"
 import H3 from "../{H3}/H3";
 import Section from "../{Section}/Section";
 import Grid from "../{Grid}/Grid";
-import type { Person } from "@/services/[Persons]/{Person}/types/Person";
 import type { Post } from "@/lib/sanity/types/Post";
 
 interface PersonsProps {
